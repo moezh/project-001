18bb02c12f438be4d24471ac975cb028985dfa3c
Moez Hachicha <moez.hachicha@icloud.com>
2024-03-18 22:42:23 +0100

    0.1.0

--- db/config.ts
+++ db/config.ts
@@ -3,6 +3,7 @@ import { defineDb, defineTable, column } from "astro:db";
 const Auth = defineTable({
   columns: {
     id: column.number({ primaryKey: true }),
+    challenge: column.text({ optional: true }),
     credential: column.text({ optional: true }),
     isActive: column.boolean({ default: true }),
   },
